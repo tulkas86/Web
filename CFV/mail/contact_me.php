ef5c07e8ee6ee0dd0325a6db18b6acd0b98ddc6e
Miguel Angel Faldutti <miguelfaldutti87@gmail.com>
2017-08-12 16:38:06 -0300

    Version 1.5

--- CFV/mail/contact_me.php
+++ CFV/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'miguelfaldutti87@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contacto@cfvsolutions.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Has recibido un nuevo mensaje enviado desde su Website.\n\n"."Detalle:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
